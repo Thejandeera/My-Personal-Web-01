150155898db05091c9118cb3ab5d91ed8358b24b
Thejandeera <thejan1010@gmail.com>
2024-07-19 23:02:37 +0530

    chat app project updated

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,12 +11,20 @@ const Projects = () => {
       desc: "The Automated Vehicle Monitoring System is an IoT project for vehicle environment and security control. It features temperature regulation, motion-based security alerts, and GPS tracking. Managed via a React.js web app, it offers real-time data and control. Powered by a rechargeable battery, future enhancements include solar power and advanced security.",
       link:"https://lnkd.in/gbgiDE6F",
     },
+   
     {
       id: 2,
       title: "Personal Web",
       img: "/myweb.png",
       desc: "I'm excited to announce my personal website launch! 🌐✨ It's currently on localhost, but you can preview it on GitHub. The site showcases my professional journey, projects, and aspirations with dynamic features and easy navigation. Your feedback is invaluable as I continue to grow.",
       link:"https://lnkd.in/gbgiDE6F",
+    },
+    {
+      id: 3,
+      title: "Chat Web",
+      img: "/chat.png",
+      desc: "  Excited to share my latest project! Built a chat web app from scratch using Express.js, MongoDB, Vite, and Tailwind CSS. Check it out and let me know what you think!",
+      link:"https://www.linkedin.com/posts/thejandeera-sandeepana-6baa17208_im-happy-to-share-this-chat-web-project-activity-7219626097158545409-cSrN?utm_source=share&utm_medium=member_androidp",
     },
     
   ];
